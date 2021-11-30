4cbe8ee0a74c2f7dc76c41812e04b2348527b04f
NikaRadosinska <nikolradosinska@gmail.com>
2021-11-30 20:20:03 +0100

    Update ConnectionSearch.java

--- src/ConnectionSearch.java
+++ src/ConnectionSearch.java
@@ -5,6 +5,9 @@ public class ConnectionSearch {
     private Lines lines;
     private Stops stops;
 
+    public ConnectionSearch(Lines lines, Stops stops){
+        this.lines = lines;
+        this.stops = stops;
     }
 
     public ConnectionData search(StopName from, StopName to, Time time){
